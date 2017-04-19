e98883441c3254ca75a48f6602b776f5f7b17582
Jiri Konecny <jkonecny@redhat.com>
2017-04-19 21:21:12 +0200

    PulseAudio port is inside of container
    PulseAudio should connect to the port which is exposed from host to
    container.
    Video is used directly.

--- docker/run_kuki.sh
+++ docker/run_kuki.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-#sudo docker run -ti --rm -e DISPLAY=unix$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v /dev/snd:/dev/snd --privileged kuki
-sudo docker run -d -v /home/$USER/.kuki:/home/kuki/.kuki -p 127.0.0.1:2222:22 kuki
+sudo docker run -ti --rm -e DISPLAY=unix$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v /home/$USER/.kuki:/home/kuki/.kuki --privileged kuki
+#sudo docker run -d -v /home/$USER/.kuki:/home/kuki/.kuki -p 127.0.0.1:2222:22 kuki
